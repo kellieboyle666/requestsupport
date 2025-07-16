ad873c9cd04c267c383b4b75543266bb4361226f
kellieboyle666 <kellie_boyle@yahoo.co.uk>
2025-07-16 18:31:16 +0100

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php 
-  header("location: https://www.bing.com/ck/a?!&&p=ef9850cb4ea18b1034596847534406fe0dc12c60919271bcf20b24d16b90cf6fJmltdHM9MTc1MjYyNDAwMA&ptn=3&ver=2&hsh=4&fclid=31ea8db5-73e1-62c6-1a28-9b9e722f6311&u=a1aHR0cHM6Ly9jYWxsaW5nLWNhcmRzLW9ubGluZS5uZXQvYWJvdXQtdXM&ntb=1");
-exit();
